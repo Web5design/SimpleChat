863ad419dc2bc48a9716a8fa0e9402b1184acc73
André Hansson <andre.hansson92@gmail.com>
2013-11-13 13:34:34 +0100

    Added license infromation

--- SimpleChat/SimpleChat/AHChatBarView.m
+++ SimpleChat/SimpleChat/AHChatBarView.m
@@ -5,6 +5,11 @@
 //  Created by André Hansson on 2013-09-24.
 //  Copyright (c) 2013 André Hansson. All rights reserved.
 //
+//  Project home page: https://github.com/PingPal/SimpleChat
+//
+//  This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License.
+//  To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/
+//
 
 #import "AHChatBarView.h"
 
--- SimpleChat/SimpleChat/Message.m
+++ SimpleChat/SimpleChat/Message.m
@@ -5,6 +5,11 @@
 //  Created by André Hansson on 2013-10-25.
 //  Copyright (c) 2013 PingPal AB. All rights reserved.
 //
+//  Project home page: https://github.com/PingPal/SimpleChat
+//
+//  This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License.
+//  To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/
+//
 
 #import "Message.h"
 #import "Friend.h"
